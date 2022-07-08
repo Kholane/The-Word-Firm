98021d06fcd9395169c4732f2a5ac02a43542105
ColeColombia <Benzemabenelzane@yahoo.com>
2022-07-08 16:43:17 +0200

    back to top button

--- js/index.js
+++ js/index.js
@@ -7,3 +7,16 @@ $(window).scroll(function () {
         $('.navbar').removeClass('sticky-top shadow-sm');
     }
 });
+
+// Back to top button
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function () {
+    $('html, body').animate({scrollTop: 0}, 500, 'easeInOutExpo');
+    return false;
+});
